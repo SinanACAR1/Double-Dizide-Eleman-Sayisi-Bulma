5721e8763d345aef4c84c194f3cc84518c4e4acb
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 21:33:10 +0300

    Add files via upload

---
+++ Double dizilerde eleman sayısı bulma/Double dizilerde eleman sayısı bulma/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[] sayılar = { 1.80, 20.30, 50.60 };
+            double eleman = sayılar.Length;
+            label1.Text = eleman.ToString(); 
+        }
+    }
+}
